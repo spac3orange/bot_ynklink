1a5b127d538eb9e6f0bec8175a8662d2c72c4dd0
spac3orange <m4gi3k44@proton.me>
2024-12-30 18:02:24 +0300

    v1.6

--- app/handlers/tarifs.py
+++ app/handlers/tarifs.py
@@ -21,19 +21,19 @@ async def tar_choose(call: CallbackQuery):
     tarif = call.data.split('_')[-1]
     if tarif == 'month':
         await call.message.answer('Месячный тариф'
-                                  '\n<b>Стоимость:</b> 2.500 тг.', reply_markup=main_kb.buy_tarif(tarif))
+                                  '\n<b>Стоимость:</b> 2.500 тг.', reply_markup=main_kb.buy_tarif(tarif), parse_mode='HTML')
         pass
     if tarif == 'quart':
         await call.message.answer('Квартальный тариф'
-                                  '\n<b>Стоимость:</b> 7.125 тг.', reply_markup=main_kb.buy_tarif(tarif))
+                                  '\n<b>Стоимость:</b> 7.125 тг.', reply_markup=main_kb.buy_tarif(tarif), parse_mode='HTML')
         pass
     if tarif == 'year':
         await call.message.answer('Годовой тариф:'
-                                  '\n\n<b>Стоимость:</b> 27.000 тг.', reply_markup=main_kb.buy_tarif(tarif))
+                                  '\n\n<b>Стоимость:</b> 27.000 тг.', reply_markup=main_kb.buy_tarif(tarif), parse_mode='HTML')
         pass
     if tarif == 'sale':
         await call.message.answer('Акционный тариф:'
-                                  '\n\n<b>Стоимость:</b> Не указана', reply_markup=main_kb.buy_tarif(tarif))
+                                  '\n\n<b>Стоимость:</b> Не указана', reply_markup=main_kb.buy_tarif(tarif), parse_mode='HTML')
         pass
 
 @router.callback_query(F.data.startswith('buy_tar_'))
