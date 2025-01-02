e6b083ac6133296a162d5e4ebb02bd37f1b975dd
spac3orange <m4gi3k44@proton.me>
2025-01-03 01:07:05 +0300

    v2.1

--- app/handlers/add_data.py
+++ app/handlers/add_data.py
@@ -8,9 +8,10 @@ from app.crud import AsyncSessionLocal
 from app.keyboards import main_kb
 from app.middlewares import album_middleware
 from app.states import states
+import os
 router = Router()
 router.message.middleware(album_middleware.AlbumMiddleware())
-
+media_folder = 'app/media'
 
 @router.callback_query(F.data == 'add_data')
 async def add_data(call: CallbackQuery, state: FSMContext):
@@ -59,14 +60,47 @@ async def p_doc(message: Message, state: FSMContext):
 
 @router.message(F.media_group_id, states.AddData.media)
 async def p_doc(message: Message, state: FSMContext, album: list = None):
-    if album:
-        count_media = len(album)
-        await message.answer(f'Спасибо за {count_media} фото')
-    else:
-        await message.answer('Спасибо за фото')
+    saved_files = []
 
-    media = message.text
-    await state.update_data(media=media)
-    data = await state.get_data()
+    if album:
+        for media_message in album:
+            file_id = None
+            file_extension = None
+
+            # Проверяем тип файла и извлекаем его ID
+            if media_message.photo:
+                file_id = media_message.photo[-1].file_id
+                file_extension = ".jpg"
+            elif media_message.video:
+                file_id = media_message.video.file_id
+                file_extension = ".mp4"
+            elif media_message.document:
+                file_id = media_message.document.file_id
+                file_extension = f".{media_message.document.file_name.split('.')[-1]}"
+
+            # Скачиваем файл, если ID найден
+            if file_id and file_extension:
+                file_path = os.path.join(media_folder, f"{file_id}{file_extension}")
+                await media_message.bot.get_file(file_id).download(destination=file_path)
+                saved_files.append(file_path)
+
+        await message.answer(f'Спасибо за {len(album)} файла(ов)')
+    else:
+        await message.answer('Медиа-группа пуста')
+
+    # Сохраняем данные о медиа
+    await state.update_data(media=saved_files)
+
+    # Выводим пути сохраненных файлов
+    for file_path in saved_files:
+        print(f"File saved: {file_path}")
+
+    # Завершаем состояние
     await state.clear()
-    await message.answer('Предпросмотр: ')
+    await message.answer("Файлы сохранены. Спасибо!")
+
+    # media = message.text
+    # await state.update_data(media=media)
+    # data = await state.get_data()
+    # await state.clear()
+    # await message.answer('Предпросмотр: ')
