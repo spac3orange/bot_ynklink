34bd0daeb8568271bbc218b93f2a5ff3718bc7b8
spac3orange <m4gi3k44@proton.me>
2025-01-05 14:56:33 +0300

    v2.3

--- app/crud/funcs.py
+++ app/crud/funcs.py
@@ -3,7 +3,9 @@ from app.crud.models import User
 from sqlalchemy.future import select
 
 
-async def add_user(session: AsyncSession, user_id: int, user_name: str) -> User:
+async def add_user(session: AsyncSession,
+                   user_id: int,
+                   user_name: str) -> User:
     # Попробуем получить пользователя с таким id
     stmt = select(User).filter_by(id=user_id)
     result = await session.execute(stmt)
@@ -19,7 +21,8 @@ async def add_user(session: AsyncSession, user_id: int, user_name: str) -> User:
     return user
 
 
-async def get_user(session: AsyncSession, user_id: int) -> User | None:
+async def get_user(session: AsyncSession,
+                   user_id: int) -> User | None:
     """
     Получить пользователя по id.
 
@@ -30,4 +33,29 @@ async def get_user(session: AsyncSession, user_id: int) -> User | None:
     stmt = select(User).filter_by(id=user_id)
     result = await session.execute(stmt)
     user = result.scalars().first()  # Получаем первого подходящего пользователя или None
+    return user
+
+
+async def update_subscription(
+    session: AsyncSession,
+    user_id: int,
+    subscription: str,
+    sub_start_date: str,
+    sub_end_date: str
+) -> User:
+
+    stmt = select(User).filter_by(id=user_id)
+    result = await session.execute(stmt)
+    user = result.scalars().first()
+
+    if user is None:
+        raise ValueError(f"Пользователь с ID {user_id} не найден.")
+
+    user.subscription = subscription
+    user.sub_start_date = sub_start_date
+    user.sub_end_date = sub_end_date
+
+    await session.commit()  # Сохраняем изменения в базе
+    await session.refresh(user)  # Обновляем объект
+
     return user
--- app/handlers/tarifs.py
+++ app/handlers/tarifs.py
@@ -3,11 +3,32 @@ from aiogram.filters import CommandStart, Command
 from aiogram import Router, F
 from aiogram.fsm.context import FSMContext
 from app.core.logger import logger
-from app.crud.funcs import add_user
+from app.crud import funcs
 from app.crud import AsyncSessionLocal
 from app.keyboards import main_kb
 from app.utils import create_payment_page, get_payment_status
+from datetime import datetime, timedelta
 router = Router()
+
+
+async def process_subscription(tarif: str):
+    sub_start_date = datetime.utcnow()
+    sub_end_date = None
+    # Определяем длительность подписки в зависимости от тарифа
+    if tarif == "month":  # 1 месяц
+        sub_end_date = sub_start_date + timedelta(days=30)
+    elif tarif == "quart":  # 3 месяца
+        sub_end_date = sub_start_date + timedelta(days=90)
+    elif tarif == "year":  # 1 год
+        sub_end_date = sub_start_date + timedelta(days=365)
+    elif tarif == "sale":  # 2
+        pass
+        # sub_end_date = sub_start_date + timedelta(days=500)
+    else:
+        raise ValueError(f"Неизвестный тариф: {tarif}")
+    sub_start_date_str = sub_start_date.strftime('%d-%m-%Y %H:%M:%S')
+    sub_end_date_str = sub_end_date.strftime('%d-%m-%Y %H:%M:%S')
+    return sub_start_date_str, sub_end_date_str
 
 @router.callback_query(F.data == 'tarif_info')
 async def tar_menu(call: CallbackQuery):
@@ -50,14 +71,27 @@ async def process_buy(call: CallbackQuery):
 @router.callback_query(F.data.startswith('get_pstatus'))
 async def get_pstatus(call: CallbackQuery):
     await call.answer()
+    uid = call.from_user.id
     pid = call.data.split('_')[-1]
     tarif = call.data.split('_')[-2]
     pstatus = await get_payment_status(pid)
+    print(pstatus)
     if pstatus in ['new', 'process', 'waiting']:
         await call.message.answer('Платеж еще обрабатывается')
     if pstatus == 'error':
         await call.message.answer('Ошибка при проведении платежа. Пожалуйста, попробуйте позднее.')
+    if pstatus == 'success':
+        await call.message.answer('Оплата прошла успешно.')
+        sub_start, sub_end = await process_subscription(tarif)
+        async with AsyncSessionLocal() as session:
+            subscription = await funcs.update_subscription(session, uid, tarif, sub_start, sub_end)
+        if subscription:
+            await call.message.answer('Добро пожаловать', reply_markup=main_kb.start_btns(True))
+        else:
+            await call.message.answer('Ошибка при обновлении данных. Пожалуйста, обратитесь в Тех. Поддержку.')
     else:
-        pass
-    print('Статус платежа:', pstatus)
+        await call.message.answer('Ошибка при обновлении данных. Пожалуйста, обратитесь в Тех. Поддержку.')
+
+
+
 
