2b396a483819df94dcac8f57c44ed07095c54e23
spac3orange <m4gi3k44@proton.me>
2025-01-08 20:26:18 +0300

    v3.1

--- app/handlers/start.py
+++ app/handlers/start.py
@@ -22,7 +22,7 @@ async def process_start(message: Message, state: FSMContext):
     print(type(uid), type(config_aiogram.admin_id))
     if user_sub:
         await message.answer('Добро пожаловать', reply_markup=main_kb.start_btns(True))
-    elif uid in config_aiogram.admin_id:
+    elif str(uid) == config_aiogram.admin_id:
         await message.answer('Добро пожаловать', reply_markup=main_kb.start_btns(True, admin=True))
     else:
         await message.answer('Вы не подписаны.', reply_markup=main_kb.start_btns(False))
