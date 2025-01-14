6d3b974111cfa32db3d0da61c9383038a62e5ae6
spac3orange <m4gi3k44@proton.me>
2025-01-14 21:43:27 +0300

    v4.5

--- app/handlers/get_data.py
+++ app/handlers/get_data.py
@@ -86,6 +86,8 @@ async def p_input_phone(message: Message, state: FSMContext):
     print(number)
     print(number.isdigit())
     if number.isdigit():
+        if not number.startswith('+'):
+            number = f"+{number}"
         async with AsyncSessionLocal() as session:
             extracted_data = await funcs.get_user_data_by_number_or_document(session, number=number)
         if extracted_data:
